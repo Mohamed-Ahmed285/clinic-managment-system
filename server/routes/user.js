c85b80700d4ab7239117271dff9cf18b2250c11c
Mohamed Ahmed <mohamed.ahmed.abdelghani6@gmail.com>
2026-07-17 11:49:41 +0300

    Implement RBAC authorization with permissions for user roles

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,18 +11,21 @@ const {
     updatePassword,
     createUser
 } = require("../controllers/user");
-const {verifyToken, isAdmin} = require("../middlewares/auth");
- 
+const {verifyToken} = require("../middlewares/auth");
+const authorize = require("../middlewares/authorize");
 
 
 router.post("/login", login);
 router.post("/logout", verifyToken, logout);
+
+
+
 router.get("/me", verifyToken, getMe);
 router.put("/me", verifyToken, updateMe);
 router.post("/forgetPassword", forgetPassword);
 router.put("/resetPassword/:token", resetPassword);
 router.put("/updatePassword", verifyToken, updatePassword);
-router.post("/createUser", verifyToken, isAdmin, createUser);
+router.post("/createUser", verifyToken, authorize("user:create"), createUser);
  
  
 module.exports = router;
