19365c37856856e445c51fecbe146d43d51653e5
Lilian <liliantarek4@gmail.com>
2026-07-22 21:10:56 +0300

    profile page

--- client/src/app/features/patient/profile/profile.component.ts
+++ client/src/app/features/patient/profile/profile.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  selectedImage: string | ArrayBuffer | null = null;
 
+  onImageSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.selectedImage = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
