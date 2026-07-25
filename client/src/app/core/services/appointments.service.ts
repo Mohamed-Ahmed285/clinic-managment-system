1ed60f30bc4f2c99e0a7d1580555091c7b5579f7
Lilian <liliantarek4@gmail.com>
2026-07-26 00:58:16 +0300

    appointments

---
+++ client/src/app/core/services/appointments.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppointmentService {
+  constructor(private http: HttpClient) {}
+
+  getAllSpecialties() {
+    return this.http.get(`${environment.apiUrl}/specialty`);
+  }
+  getDoctors(page: number, limit: number, search: string) {
+    return this.http.get(
+      `${environment.apiUrl}/doctor?page=${page}&limit=${limit}&search=${search}`,
+    );
+  }
+}
--- client/src/app/features/patient/appointments/appointments.component.ts
+++ client/src/app/features/patient/appointments/appointments.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/core/services/profile.service';
+import { AppointmentService } from 'src/app/core/services/appointments.service';
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.css'],
 })
-export class AppointmentsComponent {
-  specialties = ['All specialties', 'Cardiology', 'Dermatology', 'Dentist'];
+export class AppointmentsComponent implements OnInit {
+  search = '';
+  selectedSpecialty = '';
+  selectedState = '';
+  page = 1;
+  limit = 10;
+
+  doctors: any[] = [];
+  totalPages = 0;
+  specialties: any[] = [];
   isModalOpen = false;
+
+  profileData: any;
+  states: string[] = [
+    'Cairo',
+    'Giza',
+    'Alexandria',
+    'Dakahlia',
+    'Red Sea',
+    'Beheira',
+    'Fayoum',
+    'Gharbia',
+    'Ismailia',
+    'Menoufia',
+    'Minya',
+    'Qalyubia',
+    'New Valley',
+    'Suez',
+    'Aswan',
+    'Assiut',
+    'Beni Suef',
+    'Port Said',
+    'Damietta',
+    'Sharkia',
+    'South Sinai',
+    'Kafr El Sheikh',
+    'Matrouh',
+    'Luxor',
+    'Qena',
+    'North Sinai',
+    'Sohag',
+  ];
+  constructor(
+    private profileService: ProfileService,
+    private appointmentService: AppointmentService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProfile();
+    this.loadSpecialties();
+    this.loadDoctors();
+  }
+
+  loadProfile() {
+    this.profileService.getProfile().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.profileData = res;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
   openModal() {
     this.isModalOpen = true;
@@ -15,5 +77,30 @@ export class AppointmentsComponent {
 
   closeModal() {
     this.isModalOpen = false;
+  }
+  loadSpecialties() {
+    this.appointmentService.getAllSpecialties().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.specialties = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  loadDoctors() {
+    const searchValue = this.search || this.selectedSpecialty;
+
+    this.appointmentService
+      .getDoctors(this.page, this.limit, searchValue)
+      .subscribe({
+        next: (res: any) => {
+           console.log('Doctors Response:', res);
+          this.doctors = res;
+          this.totalPages = res.totalPages;
+        },
+        error: (err) => console.log(err),
+      });
   }
 }
