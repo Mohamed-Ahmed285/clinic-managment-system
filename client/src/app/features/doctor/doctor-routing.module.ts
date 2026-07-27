45af071e51cddddc36709da75f69b60c6a68b593
Mohamed Ahmed <mohamed.ahmed.abdelghani6@gmail.com>
2026-07-27 21:07:25 +0300

    nav bar improved

--- client/src/app/features/doctor/doctor-routing.module.ts
+++ client/src/app/features/doctor/doctor-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
 
       {
         path: 'profile',
-        component: ProfileComponent,
+        component: UpdateProfileComponent,
       },
       {
        path: 'dashboard',
