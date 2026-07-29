400c8b62adb2834bf1197cc2af9608a84aa8220f
nourashrafnada1-afk <nourashrafnada1@gmail.com>
2026-07-30 02:24:21 +0300

    Update apiUrl for development environment

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  //apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://no-q-production-9d3d.up.railway.app/payment'
+
 };
