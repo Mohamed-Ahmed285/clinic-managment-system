21537fd100eb221e297fbf2b84decb428018004b
Mohamed Ahmed <mohamed.ahmed.abdelghani6@gmail.com>
2026-07-30 04:27:36 +0300

    fix environment api uri

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:3000',
-    apiUrl: 'https://no-q-production-9d3d.up.railway.app/payment'
-
+  apiUrl: 'http://localhost:3000',
 };
